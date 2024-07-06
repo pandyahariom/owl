77cca38d0e61cfe63850cd6618724a0acf290885
Hariom Pandya <58762872+pandyahariom@users.noreply.github.com>
2024-07-06 19:33:13 +0530

    Split the logic between two components Task and Root

--- owl_todo/app.js
+++ owl_todo/app.js
@@ -1,21 +1,34 @@
 const {Component, xml, mount} = owl;
+
+class Task extends Component {
+  static template = xml /* xml */`
+    <div class="task" t-att-class="props.task_prop.isCompleted ? 'done' : ''">
+      <input type="checkbox" t-att-checked="props.task_prop.isCompleted"/>
+      <span><t t-esc="props.task_prop.text"/></span>
+    </div>`;
+    
+    // Use task_prop attribute received from Root class as a props to access the task object and its properties
+  static props = ["task_prop"];
+}
+
 class Root extends Component {
+
+  //For each item of tasks array use element(class) <Task..../> and pass the value of that array element('task') as task_prop attribute to Task class
   static template = xml`
     <div class="task-list">
       <t t-foreach="tasks" t-as="task" t-key="task.id">
-        <div class="task">
-          <input type="checkbox" t-att-checked="task.isCcompleted"/>
-          <span t-esc="task.name"/>
-        </div>
+         <Task task_prop="task"/>
       </t>
     </div>
   `
+  static components = { Task };
+
   tasks= [
-    {id: 1, name: "Learn Odoo", isCompleted: true},
-    {id: 2, name: "Learn QWeb", isCompleted: false},
-    {id: 2, name: "Learn JavaScript", isCompleted: false},
-    {id: 3, name: "Learn OWL", isCompleted: false},
+    {id: 1, text: "Learn Odoo", isCompleted: true},
+    {id: 2, text: "Learn QWeb", isCompleted: false},
+    {id: 3, text: "Learn JavaScript", isCompleted: false},
+    {id: 4, text: "Learn OWL", isCompleted: false},
   ]
 
 }
-mount(Root, document.body)
+mount(Root, document.body, {dev:true})
